4791cf4a1f589a057e9b092d4658c3bf8bef1739
vagrant <vagrant@homestead>
2018-11-19 05:37:24 +0000

    first commit

---
+++ src/Exception/Exception.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shijf
+ * Date: 2018/11/19 0019
+ * Time: 上午 11:58
+ */
+
+namespace Shijf\Weather\Exception;
+
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shijf
+ * Date: 2018/11/19 0019
+ * Time: 下午 12:00
+ */
+
+namespace Shijf\Weather\Exception;
+
+
+class InvalidArgumentException extends Exception
+{
+
+}
